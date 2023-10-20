b52326378b55f666a78fce9f1f81490cf825c71e
Mikalai Radchuk <mikalayr@gmail.com>
2023-10-20 17:58:46 +0300

    Particles pack added

---
+++ Assets/Scripts/Vfx/BloodVfx.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class BloodVfx : BaseVfx
+{
+    private ParticleSystem _particles;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        _particles = GetComponent<ParticleSystem>();
+    }
+
+    public override void Init(Vector3 position, Quaternion rotation)
+    {
+        base.Init(position, rotation);
+
+        _particles.Play();
+    }
+}
